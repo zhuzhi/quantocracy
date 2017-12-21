20b7aaf7206c835af620b681b0cfd8300dbb7602
Zhu Zhi <zhuzhi90@gmail.com>
2017-12-21 21:58:10 +0800

    fix-bug

--- src/qo_feed_get.py
+++ src/qo_feed_get.py
@@ -7,6 +7,7 @@
 # @Desc     :
 # @Contact : zhuzhi90@gmail.com
 import re
+import os
 import urllib.request
 import datetime
 from bs4 import BeautifulSoup
@@ -50,20 +51,21 @@ def qo_feed_get(overwrite=True):
 
         item_file = open(dir_sav + item_date + '.md', 'w')
 
-        file_title = link_format % (item_title.string.encode('utf-8'), item_link.string.encode('utf-8'))
+        file_title = link_format % (item_title.string, item_link.string)
 
         item_file.writelines('### ' + file_title)
         item_file.write('\n')
         item_file.writelines(item_pubDate.string)
+        item_file.write('\n')
 
         for entry in item_content.find('div', {'id': 'qo-mashup'}).find_all('div', {'class': 'qo-entry'}):
             entry_title = entry.find('a', {'class': 'qo-title'})
             entry_descr = entry.find('div', {'class': 'qo-description'})
 
-            file_entry_tile = link_format % (entry_title.string.encode('utf-8'), entry_title['href'].encode('utf-8'))
+            file_entry_tile = link_format % (entry_title.string, entry_title['href'])
             item_file.writelines('#### ' + file_entry_tile)
             item_file.write('\n')
-            item_file.writelines(entry_descr.string.encode('utf-8'))
+            item_file.writelines(entry_descr.string)
             item_file.write('\n')
 
         item_file.close()
